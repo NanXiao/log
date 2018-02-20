e821fb6820e97a57b88c15c3adfc644c5067bf72
Nan Xiao <nan@chinadtrace.org>
2018-02-20 11:13:07 +0800

    Add timestamp.

--- log.h
+++ log.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <thread>
@@ -19,8 +22,12 @@ template <typename First, typename... Rest>
 void log(std::ostream& out, First&& first, Rest&&... rest)
 {
     std::stringstream ss;
+    /* Output timestamp */
+    auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    auto ptm = std::localtime(&now);
+    ss << "[" << std::put_time(ptm, "%x %X") << ", " ;
     /* Output thread ID */
-    ss << "[0x"<<std::hex << std::this_thread::get_id() << "] "<<std::dec;
+    ss << "0x"<<std::hex << std::this_thread::get_id() << "] "<<std::dec;
     ss << std::forward<First>(first);
     log(out, ss, std::forward<Rest>(rest)...);
     return;
